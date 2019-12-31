49d7fe90ff418bbed604b79800ddfc2fae561190
Eric <wswangshuo@gmail.com>
2019-12-31 23:01:37 +0800

    spring boot basic

--- spring-exp-04-boot-basic/src/main/java/Example.java
+++ spring-exp-04-boot-basic/src/main/java/Example.java
@@ -3,6 +3,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.*;
+
 /**
  * Example
  *
