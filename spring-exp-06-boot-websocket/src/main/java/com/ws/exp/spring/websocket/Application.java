04d8285717f329be21ca336fcfa682f5f5dd4d03
iravinota <wswangshuo@gmail.com>
2020-01-19 15:34:35 +0800

    spring-boot-websocket

---
+++ spring-exp-06-boot-websocket/src/main/java/com/ws/exp/spring/websocket/Application.java
@@ -0,0 +1,17 @@
+package com.ws.exp.spring.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ *
+ * @author Eric at 2020-01-19_14:51
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ spring-exp-06-boot-websocket/src/main/java/com/ws/exp/spring/websocket/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.ws.exp.spring.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * WebSocketConfig
+ *
+ * @author Eric at 2020-01-19_14:28
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Bean
+    public MacroHandler macroHandler() {
+        return new MacroHandler();
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        // 将macorHandler()映射到 /marco
+        webSocketHandlerRegistry.addHandler(macroHandler(), "/marco");
+    }
+}
